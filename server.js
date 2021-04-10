28d2081215d3c9ca2cc43c45ed2b37b8295f1dc2
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-10 10:51:40 -0600

    Call correct database

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express'); 
 const app = express(); 
+const db = require('./queries');
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'CarryOkay'; 
@@ -8,7 +18,9 @@ app.get('/', (request, response) => {
   response.send('test');
 })
 
+
+app.get('/songs', db.getSongs)
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`)
 });
-
