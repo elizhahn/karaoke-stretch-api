e3707ef48511befce50121dda00e236407f4f975
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-12 10:10:32 -0600

    Edit server configuration to connect to deployed heroku database

--- main-server.js
+++ main-server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const {pool} = require('./config')
 const app  = express();
-const port = 8080;
-const db = require('./main-queries')
+const port = process.env.PORT || 8080;
+// const db = require('./main-queries')
 
 app.use(bodyParser.json())
 app.use(
@@ -10,13 +12,24 @@ app.use(
     extended: true
   })
 );
+app.use(cors())
+
+
+const getSongs = (request, response) => {
+  pool.query('SELECT * FROM songs', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
 app.get('/', (request, response) => {
   response.json({ test: `Home GET request is functioning properly` })
 });
 
+app.get('/songs', getSongs)
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
 })
-
-app.get('/songs', db.getSongs)
