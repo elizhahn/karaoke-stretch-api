ee757ee02d1b41fb88402da3f8159ee1376f89ef
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-13 17:53:17 -0600

    Edit heroku configuration

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const connectionString =
 `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString, ssl: isProduction, 
+  connectionString: isProduction ? "postgres://mfygnbkglmaebt:dd60e75414fbf6aef52e3a06d1465a6b229bdaed07c67abd44d1236c92a0ce1a@ec2-107-22-245-82.compute-1.amazonaws.com:5432/dequrh9vofh5kl" : connectionString, ssl: {rejectUnauthorized: false}, 
 })
 
 module.exports = {pool}
