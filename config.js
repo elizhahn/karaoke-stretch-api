6a4e4ad973401346e4ec9acec9f76a3c20e8f4b1
Marika <marikashanahan@gmail.com>
2021-04-14 11:27:06 -0600

    Define knex api resolve promises

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const {Pool} = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
+// const { Pool } = require('pg')
+// const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = 
-`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+// const connectionString = 
+// `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
-  connectionString: isProduction ? "postgres://mfygnbkglmaebt:dd60e75414fbf6aef52e3a06d1465a6b229bdaed07c67abd44d1236c92a0ce1a@ec2-107-22-245-82.compute-1.amazonaws.com:5432/dequrh9vofh5kl" : connectionString, ssl: {rejectUnauthorized: false}, 
-})
+// const pool = new Pool({
+//   connectionString: isProduction ? "postgres://mfygnbkglmaebt:dd60e75414fbf6aef52e3a06d1465a6b229bdaed07c67abd44d1236c92a0ce1a@ec2-107-22-245-82.compute-1.amazonaws.com:5432/dequrh9vofh5kl" : connectionString, ssl: {rejectUnauthorized: false}, 
+// })
 
-module.exports = {pool}
+// module.exports = {pool}
