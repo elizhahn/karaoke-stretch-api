1a0452db4e8ccb0b35976729f9284aabe93d4172
Jessica Justice <jessbjust@gmail.com>
2021-04-13 20:28:42 -0600

    Build out initial Knex files

---
+++ db/migrations/20210414021533_initial.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('songs', function (table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('artist');
+      table.string('album_cover');
+
+      table.timestamps(true, true);
+    })
+
+    .createTable('genres', function (table) {
+      table.increments('song_id').foreign();
+      table.boolean('Electronica');
+      table.boolean('Alternative');
+      table.boolean('Pop');
+      table.boolean('Dance');
+      table.boolean('Hip Hop');
+      table.boolean('Rap');
+      table.boolean('R&B');
+      table.boolean('Country');
+      table.boolean('Rock');
+      table.boolean('Metal');
+
+      table.timestamps(true, true);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('genres')
+    .dropTable('songs')
+};
